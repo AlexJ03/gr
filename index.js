a67f5398e736300231cd9317d6e9eeda95c797be
Feoktisto.v <FeoktistovSano@mail.ru>
2023-08-04 13:52:00 +0300

    Second commit

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-// Получаю все элементы html по их ID
-const a = document.getElementById("a");
-const b = document.getElementById("b");
-const c = document.getElementById("c");
-const d = document.getElementById("d");
-const e = document.getElementById("e");
+// Получаю все элементы li
+const elems = document.querySelectorAll("li");
 
-// Создаю массив объектов (el : содержит сам элемент, counter: счетчик для данного элемента)
-const arrayOfVars = [{ el: a, counter: 0 }, { el: b, counter: 0 }, { el: c, counter: 0 }, { el: d, counter: 0 }, { el: e, counter: 0 }];
+// Создаю пустой массив, в который буду добавлять объекты
+const arr = [];
 
-// Прохожу по каждому элементу массива
-arrayOfVars.forEach(item => {
-    // Вешаю событие click на каждый el
-    item.el.addEventListener("click", () => {
-        // Прибавляю counter данного элемента (++ - тоже самое что item.counter = item.counter + 1)
-        item.counter++;
-        // Дальнейшии манипуляции...
-        console.log(item.counter);
-    });
+// Прохожусь по каждому элементу elems
+elems.forEach(el => {
+
+    // Новый объект
+    const obj = {
+        el, counter: 0, dataName: el.dataset.name
+    };
+
+    // Добавляю объект в массив
+    arr.push(obj);
 })
+
+// Провека, но она тут мб и ни к чему
+if (arr.length > 0) {
+    // Прохожусь по каждому объекту массива arr
+    arr.forEach(elem => {
+
+        // Добавляю событие click на каждый el
+        elem.el.addEventListener("click", () => {
+            // Прибавляю counter
+            elem.counter++;
+
+            // Дальнейшие действия
+            console.log(elem.dataName, elem.counter);
+        })
+
+    });
+
+}
